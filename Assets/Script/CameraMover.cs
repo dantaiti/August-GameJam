c434eb22c56ef40ca80e997ea94972923ae48943
dantaiti <danbansan0421@gmail.com>
2020-08-09 15:58:07 +0900

    プロジェクト開始

---
+++ Assets/Script/CameraMover.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class CameraMover : MonoBehaviour
+{
+    private GameObject _playerob;
+    private Vector3 _offset;
+    private Vector3 _targetPos;
+    private float  h2, v2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerob = GameObject.FindWithTag("Player");
+        
+        _offset = _playerob.transform.position - this.transform.position;
+        _targetPos = _playerob.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = _targetPos - _offset;
+        _targetPos = _playerob.transform.position;
+
+        h2 = Input.GetAxis("Horizontal2");
+        v2 = Input.GetAxis("Vertical2");
+        // targetの位置のY軸を中心に、回転（公転）する
+        transform.RotateAround(_targetPos, Vector3.up, h2 * Time.deltaTime * 200f);
+        // カメラの垂直移動（※角度制限なし、必要が無ければコメントアウト）
+      //  transform.RotateAround(_targetPos, transform.right, v2 * Time.deltaTime * 200f);
+    }
+        
+    
+}
---
+++ Assets/Script/WolfNav.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using  UnityEngine.AI;
+using Random = System.Random;
+
+public class WolfNav : MonoBehaviour
+{
+    public MikataState state = default;
+
+    private NavMeshAgent _agent;
+    
+// 追いかけるキャラクター
+    [SerializeField]
+    private GameObject target;
+    private Animator _animator;
+//　到着したとする距離
+    [SerializeField]
+    private float arrivedDistance = 1.5f;
+//　追いかけ始める距離
+    [SerializeField]
+    private float followDistance = 1f;
+
+    private int _ram;
+    
+    public enum MikataState {
+        Idle, Follow, Interact
+    };
+    void Start () {
+        _animator = GetComponentInChildren<Animator> ();
+        _agent = GetComponent <NavMeshAgent> ();
+        MikataState state = MikataState.Idle;
+
+
+    }
+
+    private void Update()
+    {
+        if (state == MikataState.Follow)
+        {
+            _agent.SetDestination(target.transform.position);
+        
+            //　到着している時
+            if(_agent.remainingDistance < arrivedDistance) {
+                //agent.Stop()
+                _agent.isStopped = true;
+                _animator.SetFloat("Speed", _agent.desiredVelocity.magnitude);
+                //　到着していない時で追いかけ出す距離になったら
+            } else if(_agent.remainingDistance > followDistance) {
+                //agent.Resume();
+                //　Unity5.6バージョン以降の再開
+                _agent.isStopped = false;
+                _animator.SetFloat("Speed", _agent.desiredVelocity.magnitude);
+            }
+        }
+      
+    }
+    
+    
+    
+    
+}
