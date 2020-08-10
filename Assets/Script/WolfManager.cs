6e402384274fded6d61d221ed221b99c630ee7e8
dantaiti <danbansan0421@gmail.com>
2020-08-10 18:00:45 +0900

    manager,Spawnerを追加(未実装

--- Assets/Script/WolfManager.cs
+++ Assets/Script/WolfManager.cs
@@ -1,12 +1,15 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
+using UnityEngine.SceneManagement;
 
 public class WolfManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField]private List<GameObject> mikata;
+    [SerializeField]private List<WolfNav> allmikata = new List<WolfNav>();
+    private int _controlledWolf = 0;
     [SerializeField] private WolfContoroller contoroller;
     [SerializeField] private float selectionRadius = 1;
     private GameObject _wolf;
@@ -19,15 +22,35 @@ public class WolfManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+            Time.timeScale = Time.timeScale == 1 ? .2f : 1;
+
+        if (Input.GetKeyDown(KeyCode.R))
+            SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().name);
+
+
+        if (Input.GetMouseButton(1))
+        {
+            foreach ( WolfNav wolf in allmikata)
+            {
+                if(Vector3.Distance(wolf.transform.position, contoroller.target.position) < selectionRadius)
+                {
+                    wolf.state = WolfNav.MikataState.Follow;
+
+                }
+            }
+        }
         //Debug.Log(_wolf.gameObject.name);
-       if(Vector3.Distance(_wolf.transform.position, contoroller.target.position) < selectionRadius)
+       /*if(Vector3.Distance(_wolf.transform.position, contoroller.target.position) < selectionRadius)
        {
            _wolf.GetComponent<WolfNav>().state = WolfNav.MikataState.Follow;
-       }
+           
+       }*/
+       
     }
-    
-    
-    
-    
-    
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Debug.Log(other.gameObject.name);
+    }
 }
