589e68bafbb3cf2b488af208100f11b2b902b83b
dantaiti <danbansan0421@gmail.com>
2020-08-21 22:52:19 +0900

    navimeshの調整　UIの一部実装

---
+++ Assets/Script/UiManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Serialization;
+using  DG.Tweening;
+public class UiManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+   [SerializeField] private WolfManager catManager;
+    public TextMeshProUGUI countText;
+    void Start()
+    {
+       catManager.catFollow.AddListener((x) => UpdateCatNumber(x));
+    }
+
+    // Update is called once per frame
+
+    void UpdateCatNumber(int num)
+    {
+        countText.transform.DOComplete();
+        countText.transform.DOPunchScale(Vector3.one/3, .3f, 10, 1);
+        countText.text = num.ToString();
+    }
+    
+}
