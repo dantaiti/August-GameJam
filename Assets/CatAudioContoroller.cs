13491e3fe6613de8e28a60f96ca2686ecb353d9c
dantaiti <danbansan0421@gmail.com>
2020-08-31 15:11:30 +0900

    猫の鳴き声追加

---
+++ Assets/CatAudioContoroller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(AudioSource))]
+public class CatAudioContoroller : MonoBehaviour
+{
+    private WolfNav _cat;
+
+    private AudioSource _audioSource;
+    [Header("Sounds")]
+    public List<AudioClip> noticeSound = new List<AudioClip>();
+    // Start is called before the first frame update
+    void Awake()
+    {
+       // _cat = GetComponent<WolfNav>();
+        _audioSource = GetComponent<AudioSource>();
+        //_cat.onStartFollow.AddListener((x) => FollowSoundPlay(x));
+    }
+    // Update is called once per frame
+   public void FollowSoundPlay()
+    {
+        int i = Random.Range(0, 4);
+        Debug.Log(i);
+        _audioSource.PlayOneShot(noticeSound[i]);
+    }
+}
